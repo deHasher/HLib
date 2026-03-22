03ed730c6a49757905d5cdb08fc2a876ae87d645
deHasher <dehasher@icloud.com>
2026-03-22 20:24:38 +0300

    Улучшение хранения скинов на предметы.

--- src/main/java/net/dehasher/hlib/controller/StorageController.java
+++ src/main/java/net/dehasher/hlib/controller/StorageController.java
@@ -9,6 +9,8 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class StorageController {
+    public record ShortlySkinnedItem(String id, String name, String namespace, String category) {}
+
     @Getter
     private static final String guiNBT = "${lib_name_id}";
     @Getter
@@ -16,9 +18,11 @@ public class StorageController {
     @Getter
     private static final Set<String> ragePlayers = ConcurrentHashMap.newKeySet();
     @Getter
-    private static final Map<Player, Boolean> pvpArenaPlayers = new ConcurrentHashMap<>();
+    private static final Set<ShortlySkinnedItem> shortlySkinnedItems = ConcurrentHashMap.newKeySet();
     @Getter
-    private static final Map<HPlayer, Set<String>> skinnedItems = new ConcurrentHashMap<>();
+    private static final Map<HPlayer, Set<String>> playerSkinnedItems = new ConcurrentHashMap<>();
+    @Getter
+    private static final Map<Player, Boolean> pvpArenaPlayers = new ConcurrentHashMap<>();
     @Getter
     @Setter
     private static String noPermMessage = "";
